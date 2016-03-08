c9d53591671c54146d893bb82fbdb018e1577810
LiuJ <liujian594@hotmail.com>
2016-03-08 16:06:36 +0800

    2016-03-08

---
+++ app/src/main/java/mr_xiaoliang/com/github/lweather/bean/WeatherBean.java
@@ -0,0 +1,7 @@
+package mr_xiaoliang.com.github.lweather.bean;
+
+/**
+ * Created by LiuJ on 2016/3/8.
+ */
+public class WeatherBean {
+}
---
+++ app/src/main/java/mr_xiaoliang/com/github/lweather/view/LLineChartView.java
@@ -0,0 +1,22 @@
+package mr_xiaoliang.com.github.lweather.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * Created by LiuJ on 2016/3/8.
+ */
+public class LLineChartView extends View {
+    public LLineChartView(Context context) {
+        super(context);
+    }
+
+    public LLineChartView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public LLineChartView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
